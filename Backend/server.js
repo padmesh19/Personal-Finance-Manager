2a9376633aab21932fa61104a455ea59a03f4038
padmesh19 <padmesh191101@gmail.com>
2025-02-11 00:32:20 +0530

    deployment port changed

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,13 @@
-const mongoose = require('mongoose');
-const { MONGODB_URI, PORT } = require('./utils/config');
-const app = require('./app');
+const mongoose = require("mongoose");
+const { MONGODB_URI, PORT } = require("./utils/config");
+const app = require("./app");
 
-mongoose.connect(MONGODB_URI)
-    .then(() => {
-        console.log('Connected to the database...');
-        app.listen(`${PORT}/api/v1`, () => {
-            console.log(`Server running at http://localhost:${PORT}`);
-        });
-    })
-    .catch(err => console.log('Error connecting to the database', err));
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("Connected to the database...");
+    app.listen(PORT, () => {
+      console.log(`Server running at http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => console.log("Error connecting to the database", err));
